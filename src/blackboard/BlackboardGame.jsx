7dddb0a3ebb1b9281926173e0afbf50d22ca5fed
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2025-06-26 17:20:45 -0300

    Detecting input and checking if entered word is valid

--- src/blackboard/BlackboardGame.jsx
+++ src/blackboard/BlackboardGame.jsx
@@ -3,20 +3,39 @@ import PlayerInput from "./components/PlayerInput"
 import PlayedLetters from "./components/PlayedLetters";
 import PlayedWords from "./components/PlayedWords"
 
+import {rounds} from "./data/data";
+
 
 function BlackboardGame() {
-    return <>
-    <div>
-        <h1>
-            "The Blackboard Game"
-        </h1>
-        <div className="game-loop-container">
-            <PlayerInput />
-            <PlayedLetters />
-            <PlayedWords />
+
+    const round = rounds["1"];
+
+    function onPlayerInput({word}) {
+
+    }
+
+    function onPlayerSubmitWord({word}) {
+        const length = word.length;
+        if (round.words[length].includes(word)) {
+            console.log("SÍ")
+        }
+        else{
+            console.log("NO")
+        }
+    }
+
+    return (
+        <div>
+            <h1>
+                "The Blackboard Game"
+            </h1>
+            <div className="game-loop-container">
+                <PlayerInput onChange={onPlayerInput} onSubmit={onPlayerSubmitWord} />
+                <PlayedLetters />
+                <PlayedWords />
+            </div>
         </div>
-    </div>
-    </>
+    );
 }
 
 
--- src/blackboard/components/PlayerInput.jsx
+++ src/blackboard/components/PlayerInput.jsx
@@ -1,8 +1,29 @@
-function PlayerInput() {
+import { useState } from "react";
+
+function PlayerInput({onChange, onSubmit}) {
+
+    const [formData, setFormData] = useState({ word: "" });
+
+
+    function handleOnChange(value) {
+        setFormData(prev => {
+            return {
+                ...prev,
+                word: value.toUpperCase()
+            }
+        });
+        onChange(formData);
+    }
+
+    function handleOnSubmit(event) {
+        event.preventDefault();
+        onSubmit(formData);
+    }
+
     return (
         <div>
-            <form>
-                <input type="text" />
+            <form onSubmit={handleOnSubmit}>
+                <input type="text" value={formData.word} onChange={e => handleOnChange(e.target.value)} />
                 <input type="submit" />
             </form>
         </div>
