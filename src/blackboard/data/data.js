abaf4c7acdff08c688582e45286cd0799d05f07f
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2025-06-26 13:42:20 -0300

    Added mocking data

---
+++ src/blackboard/data/data.js
@@ -0,0 +1,47 @@
+export const rounds = {
+    1: {
+        letters: ["A", "E", "L", "M", "O", "S"],
+        words: {
+            3: ["AMO", "ESA", "ESO", "LOA", "MAL", "MES", "OLA", "OLE", "OSA", "SAL", "SOL", "MAS"],
+            4: ["LEMA", "LOMA", "LOSA", "MESA", "MALO", "MOLE", "SOLA", "ALO"],
+            5: ["SALMO"],
+            6: ["MELOSA"]
+        }
+    },
+    2: {
+        letters: ["A", "E", "L", "R", "S", "T"],
+        words: {
+            3: ["ERA", "ESA", "REA", "RES", "SAL", "SER", "TAL", "TEA"],
+            4: ["ARTE", "ESTA", "REAL", "SETA", "TELA", "TRAS", "TRES"],
+            5: ["ESTAR", "LETRA", "RESTA", "RETAL", "TELAR", "TERSA", "TESLA"],
+            6: ["LASTRE"],
+        }
+    },
+    3: {
+        letters: ["A", "A", "D", "L", "O", "S"],
+        words: {
+            3: ["ASA", "ALA", "DOS", "LOA", "ODA", "OLA", "OSA", "SAL", "SOL"],
+            4: ["LADO", "LOSA", "SALA", "SODA", "SOLA", "LASA"],
+            5: ["ALADO", "ASADO", "SALDO", "OSADA"],
+            6: ["SALADO", "SOLADA"],
+        }
+    },
+    4: {
+        letters: ["A", "E", "L", "O", "S", "T"],
+        words: {
+            3: ["ALE", "ESA", "ESO", "OLA", "OSA", "OLE", "SAL", "SOL", "TAL", "TEA", "TOS"],
+            4: ["ALTO", "ESTA", "ESTO", "LATO", "LESA", "LOSA", "LOTE", "SETA", "SETO", "SOLA", "TELA", "ALOE"],
+            5: ["SALTO", "SETAL", "TESLA"],
+            6: ["ESTOLA", "LOSETA", "SOLETA"],
+        }
+    },
+    5: {
+        letters: ["A", "E", "N", "R", "S", "T"],
+        words: {
+            3: ["ERA", "ESA", "ETA", "RAS", "REA", "RES", "SAN", "SER", "TAN", "TAS", "TEA", "TER"],
+            4: ["ARTE", "ESTA", "SETA", "TRAS", "TREN", "TRES"],
+            5: ["ANTES", "ESTAR", "RENTA", "RESTA", "SERNA", "TENSA", "TERNA", "TERSA", "TRENA"],
+            6: ["SARTEN", "SENTAR", "TENSAR"],
+        }
+    },
+}
