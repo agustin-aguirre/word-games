f3fa5a1332d172c16f4603e6f2fc9f3093ca29a3
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2025-06-27 17:26:47 -0300

    Moved data field

--- src/blackboard/data/data.js
+++ src/blackboard/data/data.js
@@ -2,61 +2,51 @@ export const rounds = {
     1: {
         letters: ["A", "E", "L", "M", "O", "S"],
         words: {
-            values: {
-                3: ["AMO", "ESA", "ESO", "LOA", "MAL", "MAS", "MES", "OLA", "OLE", "OSA", "SAL", "SOL"],
-                4: ["ALOS", "LEMA", "LOMA", "LOSA", "MALO", "MESA", "MOLE", "SOLA"],
-                5: ["SALMO"],
-                6: ["MELOSA"]
-            },
-            total: 22,
+            3: ["AMO", "ESA", "ESO", "LOA", "MAL", "MAS", "MES", "OLA", "OLE", "OSA", "SAL", "SOL"],
+            4: ["ALOS", "LEMA", "LOMA", "LOSA", "MALO", "MESA", "MOLE", "SOLA"],
+            5: ["SALMO"],
+            6: ["MELOSA"]
         },
+        total: 22,
     },
     2: {
         letters: ["A", "E", "L", "R", "S", "T"],
         words: {
-            values: {
-                3: ["ERA", "ESA", "REA", "RES", "SAL", "SER", "TAL", "TEA"],
-                4: ["ARTE", "ESTA", "REAL", "SETA", "TELA", "TRAS", "TRES"],
-                5: ["ESTAR", "LETRA", "RESTA", "RETAL", "TELAR", "TERSA", "TESLA"],
-                6: ["LASTRE"],
-            },
-            total: 22,
+            3: ["ERA", "ESA", "REA", "RES", "SAL", "SER", "TAL", "TEA"],
+            4: ["ARTE", "ESTA", "REAL", "SETA", "TELA", "TRAS", "TRES"],
+            5: ["ESTAR", "LETRA", "RESTA", "RETAL", "TELAR", "TERSA", "TESLA"],
+            6: ["LASTRE"],
         },
+        total: 22,
     },
     3: {
         letters: ["A", "A", "D", "L", "O", "S"],
         words: {
-            values: {
-                3: ["ALA", "ASA", "DOS", "LOA", "ODA", "OLA", "OSA", "SAL", "SOL"],
-                4: ["LADO", "LASA", "LOSA", "SALA", "SODA", "SOLA"],
-                5: ["ALADO", "ASADO", "OSADA", "SALDO"],
-                6: ["SALADO", "SOLADA"],
-            },
-            total: 22,
+            3: ["ALA", "ASA", "DOS", "LOA", "ODA", "OLA", "OSA", "SAL", "SOL"],
+            4: ["LADO", "LASA", "LOSA", "SALA", "SODA", "SOLA"],
+            5: ["ALADO", "ASADO", "OSADA", "SALDO"],
+            6: ["SALADO", "SOLADA"],
         },
+        total: 22,
     },
     4: {
         letters: ["A", "E", "L", "O", "S", "T"],
         words: {
-            values: {
-                3: ["ALE", "ESA", "ESO", "OLA", "OSA", "OLE", "SAL", "SOL", "TAL", "TEA", "TOS"],
-                4: ["ALOE", "ALTO", "ESTA", "ESTO", "LATO", "LESA", "LOSA", "LOTE", "SETA", "SETO", "SOLA", "TELA"],
-                5: ["SALTO", "SETAL", "TESLA"],
-                6: ["ESTOLA", "LOSETA", "SOLETA"],
-            },
-            total: 22,
+            3: ["ALE", "ESA", "ESO", "OLA", "OSA", "OLE", "SAL", "SOL", "TAL", "TEA", "TOS"],
+            4: ["ALOE", "ALTO", "ESTA", "ESTO", "LATO", "LESA", "LOSA", "LOTE", "SETA", "SETO", "SOLA", "TELA"],
+            5: ["SALTO", "SETAL", "TESLA"],
+            6: ["ESTOLA", "LOSETA", "SOLETA"],
         },
+        total: 22,
     },
     5: {
         letters: ["A", "E", "N", "R", "S", "T"],
         words: {
-            values: {
-                3: ["ERA", "ESA", "ETA", "RAS", "REA", "RES", "SAN", "SER", "TAN", "TAS", "TEA", "TER"],
-                4: ["ARTE", "ESTA", "SETA", "TRAS", "TREN", "TRES"],
-                5: ["ANTES", "ESTAR", "RENTA", "RESTA", "SERNA", "TENSA", "TERNA", "TERSA", "TRENA"],
-                6: ["SARTEN", "SENTAR", "TENSAR"],
-            },
-            total: 22,
+            3: ["ERA", "ESA", "ETA", "RAS", "REA", "RES", "SAN", "SER", "TAN", "TAS", "TEA", "TER"],
+            4: ["ARTE", "ESTA", "SETA", "TRAS", "TREN", "TRES"],
+            5: ["ANTES", "ESTAR", "RENTA", "RESTA", "SERNA", "TENSA", "TERNA", "TERSA", "TRENA"],
+            6: ["SARTEN", "SENTAR", "TENSAR"],
         },
+        total: 22,
     },
 }
