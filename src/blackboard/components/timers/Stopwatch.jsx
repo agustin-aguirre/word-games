e0f6177023fa0e81db2a1ca5900c21be55f9aaac
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2025-06-29 13:14:16 -0300

    Added stores to stopwatch and partially to player input

--- src/blackboard/components/timers/Stopwatch.jsx
+++ src/blackboard/components/timers/Stopwatch.jsx
@@ -6,25 +6,21 @@ import useRoundConfigStore from '../../stores/roundConfig';
 function Stopwatch() {
     
     const totalTime = useRoundConfigStore(state =>  state.totalTime);
-    const { timeElapsed, start, finish } = usePlayerRoundStore(state => ({
-            timeElapsed: state.timeElapsed,
-            start: state.startRound,
-            finish: state.finishRound,
-        },
-        shallow
+    const { isPlaying, timeElapsed, start, finish } = usePlayerRoundStore(state => ({
+        isPlaying: state.isPlaying,
+        timeElapsed: state.timeElapsed,
+        start: state.startRound,
+        finish: state.finishRound,
+    }, shallow
     ));
-
-    const handlePlay = (value) => {
-        value? start() : finish();
-    }
 
     return (
         <div>
             <p>Tiempo restante: {timeElapsed - totalTime}</p>
             {
-                playerRound.isPlaying ?
-                <button onClick={start()}>Detener</button> :
-                <button onClick={finsh}>Iniciar</button>
+                isPlaying
+                    ? <button onClick={start}>Detener</button>
+                    : <button onClick={finish}>Iniciar</button>
             }
         </div>
     );
