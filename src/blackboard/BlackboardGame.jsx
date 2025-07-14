622ca4e35797329de6d712a97f9ea3f21c508522
Agustín <agus.aguirre.pro@gmail.com>
2025-07-14 13:47:45 -0300

    Added game title component

--- src/blackboard/BlackboardGame.jsx
+++ src/blackboard/BlackboardGame.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import usePlayerRoundStore from "./stores/playerRound";
 import useRoundConfigStore from "./stores/roundConfig";
 import { getRoundConfig } from "./services/rounds";
+import GameTitle from "./components/titles/GameTitle";
 import Game from "./components/Game";
 import EndgameSplash from "./components/splashes/EndgameSplash";
 
@@ -24,7 +25,7 @@ function BlackboardGame() {
 
     return (
         <div className="m-0 p-0 self-center min-w-2/5 max-w-111/12 rounded-2xl">
-            <h1 className="mb-6 text-2xl text-xl-4xl font-bold text-center cantata-one-regular">El Juego del Pizarrón</h1>
+            <GameTitle value={"El Juego del Pizarrón"}/>
             <EndgameSplash show={roundState === "finished"}/>
             <Game />
             {
